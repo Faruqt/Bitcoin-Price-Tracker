c25e0e6bd8bde0026803470bfdf0c1a464693501
Faruq <faruqabdulsalam@yahoo.com>
2021-09-17 04:42:24 +0100

    Use environment to protect secret key

--- bitcoin_price/settings.py
+++ bitcoin_price/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0#ay@k8^r+xm9ui!urpx*eun!k!t2cz%-6r*#j_d7al6vz)_&@'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
