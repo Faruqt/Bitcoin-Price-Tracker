e98213a2cd439cbcf161cddb4ea6a662664ee3cf
Faruq <faruqabdulsalam@yahoo.com>
2021-09-17 13:57:28 +0100

    Add form submission test

--- price/tests.py
+++ price/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase,Client
-
 # Create your tests here.
 class formTest(TestCase):
     def test_form(self):
         client = Client()
-        response = client.post('/', {'date_from':'2021-09-07','date_to':'2021-09-16'}) #test form creation response
+        response = client.post('/', {'date_from':'2021-09-07','date_to':'2021-09-16'}) #test form  response to confrim successful form creation and submission
         self.assertEqual(response.status_code, 200)
