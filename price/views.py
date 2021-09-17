b71a61926e15ef34c11ef44dfde70f3ac62e15fc
Faruq <faruqabdulsalam@yahoo.com>
2021-09-17 07:18:22 +0100

    Adjust date range

--- price/views.py
+++ price/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def chart(request):
-    response = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-08-01&end=2021-08-10&index=[USD]')
+    response = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-09-08&end=2021-09-17&index=[USD]')
     price = response.json()
     return render(request, "btc/chart.html", {"price": price})
