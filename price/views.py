357ad3251a707b94c557f6125800356e57313a5b
Faruq <faruqabdulsalam@yahoo.com>
2021-09-17 08:43:58 +0100

    Add date filtering function

--- price/views.py
+++ price/views.py
@@ -4,6 +4,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def chart(request):
+    # price= None
+    filtered_price = {}
     response = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-09-08&end=2021-09-17&index=[USD]')
     price = response.json()
-    return render(request, "btc/chart.html", {"price": price})
+    bitcoin_price=price.get("bpi")
+
+    for key, value in bitcoin_price.items():
+        if key >= '2021-09-08' and key <= '2021-09-14':
+            filtered_price[key] = value
+            # print(filtered_price)
+    price= filtered_price
+
+    context = {
+        "price": price
+        }
+
+    return render(request, "btc/chart.html", context)
