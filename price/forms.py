46c9f2318b95674b9bcb712b2a4e90be65531959
Faruq <faruqabdulsalam@yahoo.com>
2021-09-17 11:43:49 +0100

    Adjust min and max dates on form date input fields

--- price/forms.py
+++ price/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PriceSearchForm(forms.Form):
-        date_from = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'min': '2021-09-08', 'max': '2021-09-17'}))
-        date_to = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'min': '2021-09-08', 'max': '2021-09-17'}))
+        date_from = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'min': '2021-09-07', 'max': '2021-09-16'}))
+        date_to = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'min': '2021-09-07', 'max': '2021-09-16'}))
