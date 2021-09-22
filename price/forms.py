cfbed33f30cd7f58f77500cb8ed3a7f14688ba25
Faruq <faruqabdulsalam@yahoo.com>
2021-09-22 23:46:27 +0100

    Remove date limits

--- price/forms.py
+++ price/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PriceSearchForm(forms.Form):
-        date_from = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'min': '2021-09-07', 'max': '2021-09-16'}))
-        date_to = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'min': '2021-09-07', 'max': '2021-09-16'}))
+        date_from = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+        date_to = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
