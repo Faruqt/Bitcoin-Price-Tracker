5ecbb1b0d495c84974fe6b95a5b3d054c5e8ef66
Faruq <faruqabdulsalam@yahoo.com>
2021-09-27 18:46:41 +0100

    Seperate initial form data into a function

--- price/services.py
+++ price/services.py
@@ -18,33 +18,17 @@ class getDateService():
     #function to make the api get call and retrieve the default 10days api data.
 class getDefaultData():
     def makeDefaultApiView(self, date_from, date_to):
-        initial_data={'date_from':date_from,  #set initial 10 days date range as default input
-                    'date_to':date_to,
-        }
-        search_form_default= PriceSearchForm(initial=initial_data)
-        api= 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + date_from + '&end=' + date_to + '&index=[USD]' 
-        try:
-            response = requests.get(api, timeout=10) #get api response data from coindesk based on date range supplied by user
-            response.raise_for_status()              #raise error if HTTP request returned an unsuccessful status code.
-            prices = response.json() #convert response to json format
-            default_btc_price_range=prices.get("bpi") #filter prices based on "bpi" values only
-        except requests.exceptions.ConnectionError as errc:  #raise error if connection fails
-            raise ConnectionError(errc)
-        except requests.exceptions.Timeout as errt:     #raise error if the request gets timed out after 10 seconds without receiving a single byte
-            raise TimeoutError(errt)
-        except requests.exceptions.HTTPError as err:    #raise a general error if the above named errors are not triggered 
-            raise SystemExit(err)
+        PriceFormSearch = initialData(date_from, date_to)
+        search_form_default= PriceFormSearch
 
-        return default_btc_price_range,search_form_default
+        return search_form_default
 
 class getUserInputDateRange():
     def userFormInputView(self, date_from, date_to, date_today):
         if date_to > date_today:   #if the date to from input is greater than today's date; there wont be data for the extra days, so we change the 'date_to' input back to todays's date
             date_to = date_today
-        initial_data={'date_from':date_from,   
-                    'date_to':date_to,
-                }
-        search_form_current= PriceSearchForm(initial=initial_data)  #when the page reloads, set the default form date input values to the dates picked by the user
+        PriceFormSearch = initialData(date_from, date_to)
+        search_form_current= PriceFormSearch  #when the page reloads, set the default form date input values to the dates picked by the user
 
         return  search_form_current
 class outOfRange():
@@ -55,4 +39,15 @@ class outOfRange():
         if from_date < (to_date - timedelta(days=90)):   #check if the date range is not greater than 3 months
             range_error= 'No more than 3 months data can be displayed'
 
-        return range_error
+        PriceFormSearch = initialData(date_from, date_to)
+        search_form_values= PriceFormSearch
+
+        return date_from, date_to, range_error, search_form_values
+
+def initialData(date_from, date_to):  #initial data function to render our date inputs out with chosen value
+    initial_data={'date_from':date_from,   
+                    'date_to':date_to,
+                }
+    PriceForm = PriceSearchForm(initial=initial_data)
+
+    return PriceForm
