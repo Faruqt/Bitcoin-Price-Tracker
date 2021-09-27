fc2a34e0bc45be9b860d4e058029ead905130cfc
Faruq <faruqabdulsalam@yahoo.com>
2021-09-27 18:47:30 +0100

    Move bitcoin function from service.py to views.py

--- price/views.py
+++ price/views.py
@@ -1,7 +1,7 @@
 import requests
 from django.shortcuts import render
 from .forms import PriceSearchForm
-from .services import getDateService,getDefaultData,getUserInputDateRange,outOfRange #import business logic from services.py layer
+from .services import getCurrentDateView,getDefaultData,getUserInputDateRange,outOfRange #import business logic from services.py layer
 from datetime import date, timedelta , datetime
 
 # Create your views here.
@@ -10,23 +10,27 @@ def chart(request):
     wrong_input = None
     range_error = None
 
-    initiateDateGet = getDateService()
+    # assign the functions imported from services.py to variables to allow for easier use
+    initiateDateGet = getCurrentDateView()
     initiateDefaultDataGet = getDefaultData()
     initiateUserDateGet = getUserInputDateRange()
     initiateRangeErrorGet = outOfRange()
 
-    date_from, date_to = initiateDateGet.getCurrentDateView() #get the dates for present day and present day - 10 days 
+    date_from, date_to = initiateDateGet #get the dates for present day and present day - 10 days 
 
-    bitcoin_price,search_form= initiateDefaultDataGet.makeDefaultApiView(date_from, date_to) #use the 10days period obtained from the function above to get dafualt 10days data
+    search_form= initiateDefaultDataGet.makeDefaultApiView(date_from, date_to) #use the 10days period obtained from the function above to set the default form values
+
+    bitcoin_price = getBitcoinData(date_from, date_to)#use the 10days period obtained from the function above to get dafualt 10days data
 
     from_date, to_date = getUserDateView(request) #if request method is 'post', validate the form and get date range supplied by user and use it for the api call
     
     if from_date is not None and to_date is not None:  #check if data was supplied by the user
         
-        date_out_of_range = initiateRangeErrorGet.ooR(from_date, to_date, range_error)
+        date_from, date_to, date_out_of_range, search_form = initiateRangeErrorGet.ooR(from_date, to_date, range_error)  #check if the supplied date range is not greater than 3 months
 
         if date_out_of_range is not None:
-            range_error = date_out_of_range
+            range_error = date_out_of_range  #if date range is more than 3 months, render this error in the html page
+            bitcoin_price = None
         else:
             date_today=date_to
             bitcoin_price, date_from, date_to, wrong_input = getUserInputData(from_date, to_date, date_today, wrong_input) #if there is data supplied my the user via the form, proceed to make the api call and retrieve the required data
@@ -86,3 +90,20 @@ def getUserInputData(date_from, date_to, date_today, wrong_input):
         wrong_input = 'Wrong date input selection: date from cant be greater than date to, please try again' #print out an error message if the user chooses a date that is greater than input1's date 
 
     return requested_btc_price_range, from_date, to_date , wrong_input,
+
+def getBitcoinData(date_from, date_to):
+
+    api= 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + date_from + '&end=' + date_to + '&index=[USD]' 
+    try:
+        response = requests.get(api, timeout=10) #get api response data from coindesk based on date range supplied by user
+        response.raise_for_status()              #raise error if HTTP request returned an unsuccessful status code.
+        prices = response.json() #convert response to json format
+        default_btc_price_range=prices.get("bpi") #filter prices based on "bpi" values only
+    except requests.exceptions.ConnectionError as errc:  #raise error if connection fails
+        raise ConnectionError(errc)
+    except requests.exceptions.Timeout as errt:     #raise error if the request gets timed out after 10 seconds without receiving a single byte
+        raise TimeoutError(errt)
+    except requests.exceptions.HTTPError as err:    #raise a general error if the above named errors are not triggered 
+        raise SystemExit(err)
+
+    return default_btc_price_range
